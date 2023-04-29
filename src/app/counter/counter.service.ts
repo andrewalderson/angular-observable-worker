e8c5e102d1eb7efebf210f0088eb9b546bc5f1f1
Andrew Alderson <andrew.alderson@outlook.com>
2023-04-29 19:26:57 +0000

    code cleanup

--- src/app/counter/counter.service.ts
+++ src/app/counter/counter.service.ts
@@ -8,31 +8,39 @@ import { CounterWorker } from './counter.worker';
 })
 export class CounterService {
 
-  #instance!: Remote<CounterWorker>;
+  #instance?: Remote<CounterWorker>;
 
-  #counter!: Observable<number>;
-  
+  #counter?: Observable<number>;
+
   constructor() { 
     this.init();
   }
 
   async init() {
-    const worker = wrap<typeof CounterWorker>(new Worker(new URL('./counter.worker', import.meta.url)));
-    this.#instance = await new worker();
-    
-    this.#counter = await this.#instance.counter; 
+    const counterWorker = wrap<typeof CounterWorker>(new Worker(new URL('./counter.worker', import.meta.url)));
+    this.#instance = await new counterWorker();
   }
 
   get counter() {
+    // because creating the worker is an intensive operation
+    // this will return undefined until the instance is created
+    // and we can resolve the Promise wrapping the workers 'counter'
+    // It means that components using this can't use OnPush change detection
+    // but that may not proabably not be an issue when the Signals API is available in v16
+    if(!this.#counter) {
+      this.#instance?.counter.then((value) => {
+        this.#counter = value;
+      })
+    }
     return this.#counter;
   }
 
   increment() {
-    this.#instance.increment();
+    this.#instance?.increment();
   }
 
   decrement() {
-    this.#instance.decrement();
+    this.#instance?.decrement();
   }
   
 }
